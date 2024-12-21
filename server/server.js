a818fb7fa211441f3b344961f22a63ec0d07adb3
Sushma Sarkar <123677590+SushmaSarkar26@users.noreply.github.com>
2024-12-22 01:48:28 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const colors = require("colors");
 const morgan = require("morgan");
 const cors = require("cors");
 const connectDB = require("./config/db");
-// const path = require('path')
+const path = require('path')
 //dot config
 dotenv.config();
 
@@ -15,16 +15,16 @@ connectDB();
 const app = express();
 
 //middlewares
-// app.use(express.json());
-// app.use(cors());
-app.use(cors(
-  // origin:process.env.FRONTEND_URL,
-  origin: ["https://bloodbankmanagement-eight.vercel.app"],
-  methods: ["GET","POST","PUT","DETETE"],
-  allowedHeaders: ["content-Type", "Authorization"]
-  // credentials: true
-));
 app.use(express.json());
+app.use(cors());
+// app.use(cors(
+//   // origin:process.env.FRONTEND_URL,
+//   origin: ["https://bloodbankmanagement-eight.vercel.app"],
+//   methods: ["GET","POST","PUT","DETETE"],
+//   allowedHeaders: ["content-Type", "Authorization"]
+//   // credentials: true
+// ));
+// app.use(express.json());
 app.use(morgan("dev"));
 
 //routes
@@ -36,12 +36,12 @@ app.use("/api/v1/analytics", require("./routes/analyticsRoutes"));
 app.use("/api/v1/admin", require("./routes/adminRoutes"));
 
 //STATIC FOLDER
-// app.use(express.static(path.join(__dirname,'../client/build')));
+app.use(express.static(path.join(__dirname,"../client/build")));
 
-// //STATIC ROUTES
-// app.get("*", function (Request, res){
-//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
-// })
+//STATIC ROUTES
+app.get("*", function (Request, res){
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+})
 
 app.get("/", (req,res) => {
   res.send("Welcome");
